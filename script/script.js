ed8918cf37ea85a87890e6deb7d60ff69246561f
Prabhjot Singh Sains <prabhjot.sains@gmail.com>
2023-08-07 02:51:54 -0400

    updated

---
+++ script/script.js
@@ -0,0 +1,202 @@
+
+    var library = {
+      avengers: [
+        'https://c4.wallpaperflare.com/wallpaper/293/952/22/iron-man-marvel-comics-superhero-the-avengers-wallpaper-preview.jpg',
+        'https://c4.wallpaperflare.com/wallpaper/719/640/12/marvel-comics-the-avengers-spider-man-avengers-infinity-war-wallpaper-preview.jpg',
+        'https://c4.wallpaperflare.com/wallpaper/625/108/1003/the-avengers-avengers-endgame-avengers-avengers-endgame-chris-hemsworth-hd-wallpaper-preview.jpg',
+        'https://c4.wallpaperflare.com/wallpaper/713/13/242/movies-marvel-cinematic-universe-marvel-comics-avengers-endgame-captain-america-hd-wallpaper-preview.jpg',
+        'https://c4.wallpaperflare.com/wallpaper/287/620/954/hulk-avengers-age-of-ultron-the-avengers-wallpaper-preview.jpg',
+        'https://c4.wallpaperflare.com/wallpaper/715/341/91/8k-elizabeth-olsen-avengers-infinity-war-wanda-maximoff-wallpaper-preview.jpg',
+        'https://c4.wallpaperflare.com/wallpaper/7/367/627/marvel-thor-ragnarok-tom-hiddleston-loki-wallpaper-preview.jpg',
+        'https://c4.wallpaperflare.com/wallpaper/755/464/37/the-film-scarlett-johansson-art-scarlett-johansson-wallpaper-preview.jpg',
+        'https://c4.wallpaperflare.com/wallpaper/340/197/235/doctor-strange-benedict-cumberbatch-marvel-comics-wallpaper-preview.jpg',
+        'https://c4.wallpaperflare.com/wallpaper/472/143/808/hawkeye-marvel-comics-the-avengers-jeremy-renner-wallpaper-preview.jpg',
+        'https://c4.wallpaperflare.com/wallpaper/293/952/22/iron-man-marvel-comics-superhero-the-avengers-wallpaper-preview.jpg',
+        'https://c4.wallpaperflare.com/wallpaper/719/640/12/marvel-comics-the-avengers-spider-man-avengers-infinity-war-wallpaper-preview.jpg',
+        'https://c4.wallpaperflare.com/wallpaper/625/108/1003/the-avengers-avengers-endgame-avengers-avengers-endgame-chris-hemsworth-hd-wallpaper-preview.jpg',
+        'https://c4.wallpaperflare.com/wallpaper/713/13/242/movies-marvel-cinematic-universe-marvel-comics-avengers-endgame-captain-america-hd-wallpaper-preview.jpg',
+        'https://c4.wallpaperflare.com/wallpaper/287/620/954/hulk-avengers-age-of-ultron-the-avengers-wallpaper-preview.jpg',
+        'https://c4.wallpaperflare.com/wallpaper/715/341/91/8k-elizabeth-olsen-avengers-infinity-war-wanda-maximoff-wallpaper-preview.jpg',
+        'https://c4.wallpaperflare.com/wallpaper/7/367/627/marvel-thor-ragnarok-tom-hiddleston-loki-wallpaper-preview.jpg',
+        'https://c4.wallpaperflare.com/wallpaper/755/464/37/the-film-scarlett-johansson-art-scarlett-johansson-wallpaper-preview.jpg',
+        'https://c4.wallpaperflare.com/wallpaper/340/197/235/doctor-strange-benedict-cumberbatch-marvel-comics-wallpaper-preview.jpg',
+        'https://c4.wallpaperflare.com/wallpaper/472/143/808/hawkeye-marvel-comics-the-avengers-jeremy-renner-wallpaper-preview.jpg'
+      ],
+      starwars: [
+        'https://res.cloudinary.com/beumsk/image/upload/v1547980981/memory/starwars/anakin%20skywalker.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981009/memory/starwars/luke%20skywalker.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981022/memory/starwars/Obi%20wann.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981054/memory/starwars/Han%20solo.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981074/memory/starwars/chewbacca.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981095/memory/starwars/yoda.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981117/memory/starwars/dark%20sidious.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981141/memory/starwars/dark%20vador.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981165/memory/starwars/padme.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981190/memory/starwars/leia.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547980981/memory/starwars/anakin%20skywalker.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981009/memory/starwars/luke%20skywalker.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981022/memory/starwars/Obi%20wann.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981054/memory/starwars/Han%20solo.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981074/memory/starwars/chewbacca.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981095/memory/starwars/yoda.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981117/memory/starwars/dark%20sidious.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981141/memory/starwars/dark%20vador.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981165/memory/starwars/padme.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981190/memory/starwars/leia.jpg'
+      ],
+      lotr: [
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981408/memory/lotr/gandalf.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981438/memory/lotr/sauron.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981469/memory/lotr/Aragorn.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981501/memory/lotr/legolas.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981535/memory/lotr/Gimli.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981603/memory/lotr/golum.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981645/memory/lotr/sam.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981686/memory/lotr/saroumane.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981738/memory/lotr/bilbo.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981802/memory/lotr/frodo.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981408/memory/lotr/gandalf.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981438/memory/lotr/sauron.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981469/memory/lotr/Aragorn.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981501/memory/lotr/legolas.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981535/memory/lotr/Gimli.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981603/memory/lotr/golum.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981645/memory/lotr/sam.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981686/memory/lotr/saroumane.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981738/memory/lotr/bilbo.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547981802/memory/lotr/frodo.jpg'
+      ],
+      disney: [
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982044/memory/disney/mickey.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982088/memory/disney/mowgli.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982610/memory/disney/tarzan.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982620/memory/disney/simba.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982628/memory/disney/aladin.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982636/memory/disney/blanche%20neige.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982644/memory/disney/alice.png',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982653/memory/disney/peter%20pan.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982663/memory/disney/pinocchio.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982738/memory/disney/raiponce.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982044/memory/disney/mickey.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982088/memory/disney/mowgli.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982610/memory/disney/tarzan.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982620/memory/disney/simba.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982628/memory/disney/aladin.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982636/memory/disney/blanche%20neige.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982644/memory/disney/alice.png',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982653/memory/disney/peter%20pan.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982663/memory/disney/pinocchio.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982738/memory/disney/raiponce.jpg'
+      ]
+    };
+    
+    var images = [];
+    var tempElt1 = "";
+    var tempElt2 = "";
+    var click = -1;
+    var win = 0;
+    var score = 0;
+    var time = 0;
+    var timer;
+
+    var selection = document.getElementById("selection");
+    var movies = document.getElementById("movies");
+    var img = document.getElementsByClassName("img");
+    var gameBoard = document.querySelector(".gameBoard");
+    var gametime = document.getElementById("time");
+    var gamescore = document.getElementById("score");
+    var result = document.getElementById("result");
+    var resultFinal = document.getElementById("resultFinal");
+    var playAgain = document.getElementById("playAgain");
+    
+    movies.addEventListener("click", function (e) {
+      if (e.target.classList.contains("movies")) {
+        activateTheme(e.target.id);
+        selection.classList.add("hidden");
+        gameBoard.classList.remove("hidden");
+      }
+    });
+    
+    function activateTheme(theme) {
+      for (let i = 0; i < 20; i++) {
+        images.push(library[theme][i]);
+      }
+    
+      for (let i = 0; i < 20; i++) {
+        var rand = Math.floor(Math.random() * images.length);
+        img[i].innerHTML = "<img src='" + images[rand] + "' alt='image' class='hidden'>";
+        images.splice(rand, 1);
+      }
+    }
+  
+    gameBoard.addEventListener("click", gameLogic);
+    
+    function gameLogic(e) {
+      if (e.target.classList.contains("flip")) {
+        e.target.firstChild.classList.remove("hidden");
+        if (click < 1) {
+          if (click === -1) {
+            timer = setInterval(function () {
+              time++;
+              gametime.innerHTML = time;
+            }, 1000);
+          }
+          click = 1;
+          tempElt1 = e.target; // Move assignment here to avoid issues
+        } else if (e.target !== tempElt1) {
+          tempElt2 = e.target;
+          if (tempElt1.firstChild.src !== tempElt2.firstChild.src) {
+            gameBoard.removeEventListener("click", gameLogic);
+            setTimeout(function () {
+              tempElt1.firstChild.classList.add("hidden");
+              tempElt2.firstChild.classList.add("hidden");
+              gameBoard.addEventListener("click", gameLogic);
+            }, 400);
+            if (score > 0) {
+              score -= 2;
+            }
+            gamescore.innerHTML = score;
+          } else {
+            score += 10;
+            win += 2;
+            tempElt1.firstChild.classList.add("outline");
+            tempElt2.firstChild.classList.add("outline");
+            tempElt1.classList.remove("flip");
+            tempElt2.classList.remove("flip");
+            score.innerHTML = score;
+    
+            if (win === 20) {
+              clearInterval(timer);
+              resultFinal.innerHTML = "You won " + score + " points <br> in " + time + " seconds";
+              result.classList.remove("hidden");
+            }
+          }
+          click = 0;
+        }
+      }
+    }
+    
+    playAgain.addEventListener("click", resetGame);
+    
+    function resetGame() {
+      tempElt1 = "";
+      tempElt2 = "";
+      click = -1;
+      win = 0;
+      score = 0;
+      time = 0;
+      clearInterval(timer);
+      result.classList.add("hidden");
+      gameBoard.classList.add("hidden"); // Hide the main section
+      selection.classList.remove("hidden"); // Show the selection menu
+      for (let i = 0; i < 20; i++) {
+        img[i].classList.add("flip");
+        img[i].firstChild.classList.add("hidden");
+      }
+      gametime.textContent = time;
+      gamescore.textContent = score; // Use scoreElement to update the score
+    }
+    
+    
+    
+    
